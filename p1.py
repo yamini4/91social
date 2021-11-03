2dd851f79435587dad4c82a44090b19ac31548af
yamini4 <85215465+yamini4@users.noreply.github.com>
2021-11-03 20:38:27 +0530

    Delete p1.py

--- p1.py
+++
@@ -1,7 +0,0 @@
-# python program to read a file line by line and store it into a list
-def reading_file():
-    list1 = open("C:\\users\\desktop\\intern\\sample.txt", "r")
-    print(list1.readlines())
-  
-  
-reading_file()
